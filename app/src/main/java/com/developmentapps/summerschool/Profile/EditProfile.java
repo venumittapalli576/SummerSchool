1ac025f1be3612decb4a8cd30196b03aef18f1da
venu <venumittapalli7@gmail.com>
2019-03-06 15:41:07 +0530

    Added update profile

--- app/src/main/java/com/developmentapps/summerschool/Profile/EditProfile.java
+++ app/src/main/java/com/developmentapps/summerschool/Profile/EditProfile.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,6 +20,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.developmentapps.summerschool.Register.memberRegister.MySingleton;
 import com.developmentapps.summerschool.Register.memberRegister.SessionHandler;
+import com.developmentapps.summerschool.Register.memberRegister.User;
 import com.developmentapps.summerschool.activity.MainActivity;
 
 import org.json.JSONException;
@@ -38,7 +40,7 @@ public class EditProfile extends AppCompatActivity {
     private static final String KEY_LOCATION = "Location";
     private static final String KEY_PHONENUMBER = "Phonenumber";
     private static final String KEY_EMPTY = "";
-    private EditText etUsername;
+    private TextView etUsername;
     private EditText etPassword;
     private EditText etConfirmPassword;
     private EditText etFullName;
@@ -63,8 +65,9 @@ public class EditProfile extends AppCompatActivity {
     private ProgressDialog pDialog;
     Button update;
     //private String register_url = "http://172.168.2.78/summerportal/register.php";
-    private String register_url = "http://192.168.43.108/summerportal/update.php";
+    private String register_url = "http://192.168.43.240/summerportal/update.php";
     private SessionHandler session;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,17 +75,22 @@ public class EditProfile extends AppCompatActivity {
         session = new SessionHandler(getApplicationContext());
         setContentView(R.layout.activity_edit_profile);
 
+        user=session.getUserDetails();
+
         etUsername = findViewById(R.id.etusername);
-        etPassword = findViewById(R.id.etPassword);
-        etConfirmPassword = findViewById(R.id.etConfirmPassword);
-        etFullName = findViewById(R.id.etFullName);
+        etUsername.setText(user.getUsername());
         etEmail = findViewById(R.id.etmail);
-        etPhonenumber=findViewById(R.id.etphone);
+
+       /* etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        etFullName = findViewById(R.id.etFullName);*/
+
+       /* etPhonenumber=findViewById(R.id.etphone);
         etFathername=findViewById(R.id.etFathername);
         etAge=findViewById(R.id.etAge);
         etIntesteredcourse=findViewById(R.id.etSelectedcourse);
         etAddress=findViewById(R.id.etAddress);
-        etLocation=findViewById(R.id.etLocation);
+        etLocation=findViewById(R.id.etLocation);*/
 
        update = findViewById(R.id.btnUpdate);
 
@@ -93,16 +101,16 @@ public class EditProfile extends AppCompatActivity {
             public void onClick(View v) {
                 //Retrieve the data entered in the edit texts
                 username = etUsername.getText().toString().toLowerCase().trim();
-                password = etPassword.getText().toString().trim();
+               /* password = etPassword.getText().toString().trim();
                 confirmPassword = etConfirmPassword.getText().toString().trim();
-                fullName = etFullName.getText().toString().trim();
+                fullName = etFullName.getText().toString().trim();*/
                 Email=etEmail.getText().toString().trim();
-                Phonenumber=etPhonenumber.getText().toString().trim();
+               /* Phonenumber=etPhonenumber.getText().toString().trim();
                 Fathername=etFathername.getText().toString().trim();
                 Age=etAge.getText().toString().trim();
                 Intesteredcourse=etIntesteredcourse.getText().toString().trim();
                 Address=etAddress.getText().toString().trim();
-                Location=etLocation.getText().toString().trim();
+                Location=etLocation.getText().toString().trim();*/
                 if (validateInputs()) {
                     UpdateUser();
                 }
@@ -128,9 +136,7 @@ public class EditProfile extends AppCompatActivity {
      * Launch Dashboard Activity on Successful Sign Up
      */
     private void loadDashboard() {
-        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(i);
-        finish();
+       Toast.makeText(getApplicationContext(),"updted Successfully",Toast.LENGTH_LONG).show();
 
     }
 
@@ -140,15 +146,15 @@ public class EditProfile extends AppCompatActivity {
         try {
             //Populate the request parameters
             request.put(KEY_USERNAME, username);
-            request.put(KEY_PASSWORD, password);
-            request.put(KEY_FULL_NAME, fullName);
+           // request.put(KEY_PASSWORD, password);
+           // request.put(KEY_FULL_NAME, fullName);
             request.put(KEY_EMAIL, Email);
-            request.put(KEY_PHONENUMBER, Phonenumber);
+           /* request.put(KEY_PHONENUMBER, Phonenumber);
             request.put(KEY_FATHERNAME, Fathername);
             request.put(KEY_AGE, Age);
             request.put(KEY_INTERESTEDCOURSE, Intesteredcourse);
             request.put(KEY_ADDRESS, Address);
-            request.put(KEY_LOCATION, Location);
+            request.put(KEY_LOCATION, Location);*/
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -160,15 +166,10 @@ public class EditProfile extends AppCompatActivity {
                         pDialog.dismiss();
                         try {
                             //Check if user got registered successfully
-                            if (response.getInt(KEY_STATUS) == 0) {
+                            if (response.getInt(KEY_STATUS) != 0) {
                                 //Set the user session
-                                session.loginUser(username,fullName,Email,Phonenumber,Intesteredcourse,Fathername,Location,Address,Age);
+                                session.loginUser(username, response.getString(KEY_FULL_NAME), Email, response.getString(KEY_PHONENUMBER), response.getString(KEY_INTERESTEDCOURSE),response.getString(KEY_FATHERNAME),response.getString(KEY_LOCATION),response.getString(KEY_ADDRESS),response.getString(KEY_AGE));
                                 loadDashboard();
-
-                            }else if(response.getInt(KEY_STATUS) == 1){
-                                //Display error message if username is already existsing
-                                etUsername.setError("Username already taken!");
-                                etUsername.requestFocus();
 
                             }else{
                                 Toast.makeText(getApplicationContext(),
